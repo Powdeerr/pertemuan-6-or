e4711f2db6eed3e31641ccd834cb46661742a410
rapipn02 <rapipn02@gmail.com>
2026-06-05 19:48:45 +0700

    1st commit

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
     success: true,
     message: "📚 Selamat datang di API Manajemen Buku!",
     info: {
-      docs: `http://localhost:${PORT}/api-docs`,
+      docs: `pertemuan-6-or-production.up.railway.app/api-docs`,
       version: "1.0.0",
       bootcamp: "OR 15 — Neo Telemetri",
     },
--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
+        url: `pertemuan-6-or-production.up.railway.app`,
       },
     ],
     components: {
